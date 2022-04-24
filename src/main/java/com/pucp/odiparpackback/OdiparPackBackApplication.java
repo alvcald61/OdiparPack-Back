980b9936d5b1f32d89dab9ed70a002e4b6003494
Alvaro Calderon <alvaro.calderon@pucp.edu.pe>
2022-04-24 13:30:58 -0500

    Initial commit

---
+++ src/main/java/com/pucp/odiparpackback/OdiparPackBackApplication.java
@@ -0,0 +1,13 @@
+package com.pucp.odiparpackback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdiparPackBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OdiparPackBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/pucp/odiparpackback/OdiparPackBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucp.odiparpackback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdiparPackBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
