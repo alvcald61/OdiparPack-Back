4f1166f8b40270096629a6c2a83270fb9914020a
Alvaro Calderon <alvaro.calderon@pucp.edu.pe>
2022-05-29 21:57:44 -0500

    change DB

--- src/main/java/com/pucp/odiparpackback/config/FileLoader.java
+++ src/main/java/com/pucp/odiparpackback/config/FileLoader.java
@@ -12,8 +12,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
-
 import static org.apache.lucene.util.SloppyMath.haversinMeters;
 
 @Component
@@ -41,54 +39,57 @@ class FileLoader implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
+    /*
     BufferedReader reader;
-//    logger.info("Cargando archivos de datos de ciudades");
-//    try {
-//      reader = new BufferedReader(new FileReader("D:/JavaProjects/OdiparPack-Back/src/main/resources/files/inf226.oficinas.txt"));
-//      do {
-//        String line = reader.readLine();
-//        if (line == null) {
-//          break;
-//        }
-//        logger.info("{}", line);
-//        String[] split = line.split(",");
-//        City city = new City();
-//        city.setUbigeo(split[0]);
-//        city.setName(split[2]);
-//        city.setLatitude(split[3]);
-//        city.setLongitude(split[4]);
-//        if (split[5].equals("SELVA")) {
-//          city.setRegion(Region.JUNGLE);
-//        }
-//        if (split[5].equals("COSTA")) {
-//          city.setRegion(Region.COST);
-//        }
-//        if (split[5].equals("SIERRA")) {
-//          city.setRegion(Region.HIGHLAND);
-//        }
-//        cityRepository.save(city);
-//        logger.info("Cities loaded");
-//      } while (true);
-//    } catch (Exception e) {
-//      logger.error("Error al cargar los archivos de datos", e);
-//    }
-//    logger.info("loading depots");
-//    Depot depot = new Depot();
-//    depot.setCity(cityRepository.findByUbigeo("040101"));
-//    depot.setName("Almacén de Arequipa");
-//    depotRepository.save(depot);
-//
-//    Depot depot2 = new Depot();
-//    depot2.setCity(cityRepository.findByUbigeo("150101"));
-//    depot2.setName("Almacén de Arequipa");
-//    depotRepository.save(depot2);
-//
-//    Depot depot3 = new Depot();
-//    depot3.setCity(cityRepository.findByUbigeo("130101"));
-//    depot3.setName("Almacén de Arequipa");
-//    depotRepository.save(depot3);
-//    logger.info("depots loaded");
-//    logger.trace("loading Routes");
+    logger.info("Cargando archivos de datos de ciudades");
+    try {
+      reader = new BufferedReader(new FileReader("D:/JavaProjects/OdiparPack-Back/src/main/resources/files/inf226.oficinas.txt"));
+      do {
+        String line = reader.readLine();
+        if (line == null) {
+          break;
+        }
+        logger.info("{}", line);
+        String[] split = line.split(",");
+        City city = new City();
+        city.setUbigeo(split[0]);
+        city.setName(split[2]);
+        city.setLatitude(split[3]);
+        city.setLongitude(split[4]);
+        if (split[5].equals("SELVA")) {
+          city.setRegion(Region.JUNGLE);
+        }
+        if (split[5].equals("COSTA")) {
+          city.setRegion(Region.COST);
+        }
+        if (split[5].equals("SIERRA")) {
+          city.setRegion(Region.HIGHLAND);
+        }
+        cityRepository.save(city);
+        logger.info("Cities loaded");
+      } while (true);
+    } catch (Exception e) {
+      logger.error("Error al cargar los archivos de datos", e);
+    }
+    logger.info("loading depots");
+    Depot depot = new Depot();
+    depot.setCity(cityRepository.findByUbigeo("040101"));
+    depot.setName("Almacén de Arequipa");
+    depotRepository.save(depot);
+
+    Depot depot2 = new Depot();
+    depot2.setCity(cityRepository.findByUbigeo("150101"));
+    depot2.setName("Almacén de Arequipa");
+    depotRepository.save(depot2);
+
+    Depot depot3 = new Depot();
+    depot3.setCity(cityRepository.findByUbigeo("130101"));
+    depot3.setName("Almacén de Arequipa");
+    depotRepository.save(depot3);
+    logger.info("depots loaded");
+    logger.trace("loading Routes");
+    */
+
 
 //    try {
 //      reader = new BufferedReader(new FileReader("D:/JavaProjects/OdiparPack-Back/src/main/resources/files/inf226.tramos.v.2.0.txt"));
