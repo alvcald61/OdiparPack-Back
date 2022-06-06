a7331781a2059aceae2e3acb31986b9daeba7761
Robert <robert.chirinos@pucp.edu.pe>
2022-06-06 16:55:51 -0500

    Agrega codigo de estado

--- src/main/java/com/pucp/odiparpackback/controller/json/ResponseJson.java
+++ src/main/java/com/pucp/odiparpackback/controller/json/ResponseJson.java
@@ -1,19 +1,28 @@
 package com.pucp.odiparpackback.controller.json;
 
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 @Data
 public class ResponseJson<T> {
   private ErrorJson error;
   private T data;
+  private HttpStatus status;
 
   public ResponseJson(ErrorJson error) {
     this.error = error;
     this.data = null;
   }
 
+  public ResponseJson(ErrorJson error, HttpStatus status) {
+    this.error = error;
+    this.data = null;
+    this.status = status;
+  }
+
   public ResponseJson(T data) {
     this.data = data;
     this.error = null;
+    this.status = HttpStatus.OK;
   }
 }
