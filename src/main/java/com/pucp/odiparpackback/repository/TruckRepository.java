28afe429a2ccd32360aed538a668edd9a17d4722
Robert <robert.chirinos@pucp.edu.pe>
2022-06-26 19:04:18 -0500

    Correccion truckRepository

--- src/main/java/com/pucp/odiparpackback/repository/TruckRepository.java
+++ src/main/java/com/pucp/odiparpackback/repository/TruckRepository.java
@@ -9,8 +9,6 @@ import java.util.List;
 
 @Repository
 public interface TruckRepository extends JpaRepository<Truck, Long> {
-  List<Truck> findByAvailableTrue();
-
   Truck findTruckByCode(String code);
 
   List<Truck> findAllByStatusLessThanEqual(TruckStatus status);
