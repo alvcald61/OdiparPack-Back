dc0a006319ba848e32c8bcaf7dd78d7b7052f0a8
Robert <robert.chirinos@pucp.edu.pe>
2022-06-30 09:43:57 -0500

    Elimina clases no usadas

--- src/main/java/com/pucp/odiparpackback/repository/UploadFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pucp.odiparpackback.repository;
-
-import com.pucp.odiparpackback.enums.FileType;
-import com.pucp.odiparpackback.model.UploadFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UploadFileRepository extends JpaRepository<UploadFile, Long> {
-  UploadFile findOneByFileTypeOrderByUploadDateDesc(FileType fileType);
-}
